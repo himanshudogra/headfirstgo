fe1f91363e7d2929a3dc78b665837ac5834afff8
himanshu <himanshudogra96@gmail.com>
2020-08-04 10:19:39 +0530

    Update main.go

--- count/main.go
+++ count/main.go
@@ -13,5 +13,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(lines)
+	var names []string
+	var counts []int
+
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+
+		}
+
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+
+	}
+	for i, name := range names {
+		fmt.Printf("%s: %5d\n", name, counts[i])
+	}
 }
